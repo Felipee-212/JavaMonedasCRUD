c11cbde23f56a4f327901a1f4dd71bba96097356
Felipee-212 <felipegula120@gmail.com>
2025-11-17 21:21:30 -0300

    version temporal repository

--- src/main/java/com/example/javamonedascrud/repository/MonedaRepo.java
+++ src/main/java/com/example/javamonedascrud/repository/MonedaRepo.java
@@ -1,6 +1,6 @@
-package repository;
+package com.example.javamonedascrud.repository;
 
-import model.Moneda;
+import com.example.javamonedascrud.model.Moneda;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MonedaRepo extends JpaRepository<Moneda,Long> {
